f60a913af17b51d1fe8778fa33de84c743baedeb
David Ögaard Jozic <davidsstudier@gmail.com>
2024-05-08 05:10:40 +0200

    små ändringar

--- imageAndBow.R
+++ imageAndBow.R
@@ -4,30 +4,7 @@ library(spatstat)
 load("Bow.RData")
 painting <- readJPEG("P4.jpg")
 
-
-# data detaljer
-missingValsIdxNA <- which(is.na(Bow$presacinpic))
-goodDataIdx <- which(!is.na(Bow$presacinpic))
-missingValsIdx <- missingValsIdxNA[-1] ### la till den här raden
-### missingVals till for-loopen fick inte innehålla rad 1 för då funkade inte max
-
-for (i in missingValsIdx) {
-  # Hittar största värdet innan NaN
-  prevValidIdx <- max(goodDataIdx[goodDataIdx < i])
-  
-  # Adderar det värdet med det "ite" missingValsIdx
-  Bow$duration[prevValidIdx] <- Bow$duration[i] + Bow$duration[prevValidIdx]
-  Bow$presac[prevValidIdx] <- Bow$presac[i] + Bow$presac[prevValidIdx]
-}
-Bow <- Bow[-missingValsIdxNA, ] ## tar bort avsaknade värden ### la till NA här
-
-Bow <- Bow[-1, ] # ta bort 1a element, saknar fixering
-
-
-
 Bow <- subset(Bow, Bow$timestamp <= 60000)
-Bow <-na.omit(Bow)
-
 
 # bild detaljer
 window <- owin(c(114, 908), c(0, 767))
